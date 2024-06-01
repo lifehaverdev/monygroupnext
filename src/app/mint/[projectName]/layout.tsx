1ae3e326461c313856b3d670b25ce213521a10c2
jfkwebdev <jfkwebdev@gmail.com>
2024-06-01 15:41:10 -0500

    walletconnect configure

--- src/app/mint/[projectName]/layout.tsx
+++ src/app/mint/[projectName]/layout.tsx
@@ -1,42 +1,64 @@
 'use client'
 
+import React, { ReactNode } from 'react'
+import type { Metadata } from 'next'
+import { headers } from 'next/headers'
+import { createWeb3Modal } from '@web3modal/wagmi'
 import '@rainbow-me/rainbowkit/styles.css';
 import {
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
+import { State, WagmiProvider } from 'wagmi';
 import {
   QueryClientProvider,
   QueryClient,
 } from "@tanstack/react-query";
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { config } from '../_config';
+import { config, projectId } from '../_config';
 import Header from '../../../components/header'
+import { cookieToInitialState } from 'wagmi'
 
 const queryClient = new QueryClient();
+if (!projectId) throw new Error('Project ID is not defined')
+
+  // Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
+})
+
+export const metadata: Metadata = {
+  title: 'Mony Group Mint Platform',
+  description: 'Mint NFTs from'
+}
+
 
 export default function ProjectLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-        <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-            <RainbowKitProvider>
-                <Header navigation={
-                    [
-                        {name: 'Lore', href: '/lore'},
-                        {name: 'Merch', href: '/merch'},
-                        {name: 'Mint', href: '/mint'}
-                    ]}
-                    rightElement = {<ConnectButton/>}
-                />
-                {/* <ConnectButton /> */}
-                {children}
-            </RainbowKitProvider>
-        </QueryClientProvider>
-        </WagmiProvider>
-    )
-  }
+  children
+}: {
+  children: ReactNode
+  initialState?: State
+}) {
+  const initialState = cookieToInitialState(config, headers().get('cookie'))
+  return (
+    <WagmiProvider config={config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Header navigation={
+              [
+                  {name: 'Lore', href: '/lore'},
+                  {name: 'Merch', href: '/merch'},
+                  {name: 'Mint', href: '/mint'}
+              ]}
+              rightElement = {<ConnectButton/>}
+          />
+          {/* <ConnectButton /> */}
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
--- src/app/mint/_config.tsx
+++ src/app/mint/_config.tsx
@@ -1,24 +1,45 @@
 'use client'
 
-import {
-    getDefaultConfig
-  } from '@rainbow-me/rainbowkit';
+// import {
+//     getDefaultConfig
+//   } from '@rainbow-me/rainbowkit';
 import { http } from 'viem';
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { cookieStorage, createStorage } from 'wagmi'
 
 import {
 mainnet,
+sepolia,
 polygon,
 optimism,
 arbitrum,
 base,
 } from 'wagmi/chains';
 
-export const config = getDefaultConfig({
-    appName: 'Mony Mint',
-    projectId: '4444444',
-    chains: [mainnet],//, base],
-    transports: {
-      [mainnet.id]: http('https://eth-mainnet.g.alchemy.com/v2/umImd99xY47XFAHi4JpUPqCLjUQV_jgc'),
-      //[base.id]: http('https://eth-sepolia.g.alchemy.com/v2/...'),
-    },
-  });
+// Your WalletConnect Cloud project ID
+export const projectId = '815947931436f8a2b677d1d27dc02ec2'
+
+// Create a metadata object
+const metadata = {
+  name: 'MonyGroup',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+  transports: {
+    [mainnet.id]: http('https://eth-mainnet.g.alchemy.com/v2/umImd99xY47XFAHi4JpUPqCLjUQV_jgc'),
+    //[base.id]: http('https://eth-sepolia.g.alchemy.com/v2/...'),
+  },
+})
